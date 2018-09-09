1ff51235bc27adb903df0bc9b7e0b21d2293e5f6
andrey <brazhenko>
2018-09-09 16:57:22 +0300

    добавлено взаимодействие через консоль

--- gen.py
+++ gen.py
@@ -1,41 +1,6 @@
-'''
-Задание только в начальной стадии разработки. Обучался на текстах Войны и Мира.
-'''
-
 import re
 import json
 import random
-
-
-'''def get_words(text):
-    return re.compile('\w+').findall(text)
-
-res = (open('learn.txt', 'r').read())
-res = get_words(res)
-Dictionary = {}
-
-
-for word in res:
-    if word not in Dictionary:
-        Dictionary.update([(word, [])])
-
-
-for i in range(len(res)-1):
-    Dictionary[res[i]] = Dictionary[res[i]] + [res[i+1]]
-
-
-def generate(l, start):
-    text = [start]
-    start = start
-    for i in range(l):
-        num = random.randint(0, len(Dictionary[start])-1)
-        text = text + [Dictionary[start][num]]
-        start = Dictionary[start][num]
-    return text
-
-test_txt = ' '.join(generate(100, 'охота'))
-print(text_txt)
-'''
 
 
 class TextGenerator(object):
@@ -62,13 +27,37 @@ class TextGenerator(object):
             num = random.randint(0, len(self.Dictionary[start]) - 1)
             text = text + [self.Dictionary[start][num]]
             start = self.Dictionary[start][num]
-        return ' '.join(text)
+        return ' '.join(text) + '.'
 
     def dump(self):
         with open('pairs.json', "w", encoding="utf-8") as file:
             json.dump(self.Dictionary, file)
 
-a = TextGenerator()
-a.fit()
-print(a.generate(10, ))
-a.dump()
+
+if __name__ == '__main__':
+    print("Приветствую пользователя моей программы. \n Для начала работы загрузите текст в learn.txt и напишите learn.\n Далее напишите длину последовательности в словах, \n которую вам требуется сгенерировать. \n Для выхода нажмите q")
+
+    while True:
+        command = input('Введите команду: ')
+        if command == 'q':
+            break
+        elif command == 'learn':
+            a = TextGenerator()
+            a.fit()
+            print('Обучение закончено')
+        else:
+            print('Неверный ввод')
+            continue
+
+        while True:
+            command = input('Можно ввести длину или вернуться к обучению(написать back) ')
+            if command == 'back':
+                break
+            try:
+                number = int(command)
+            except Exception:
+                print('Неверный ввод')
+                continue
+
+            print(a.generate(number))
+            # a.dump()
